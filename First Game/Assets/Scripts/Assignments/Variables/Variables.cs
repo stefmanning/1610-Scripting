8c5e1306a9b8d7dc78262777852e7025b1073534
Stef <stefani.olson@gmail.com>
2020-01-15 17:19:31 -0700

    Class updates on Unity and VS
    In class we learned functions and variables and updated script accordingly

--- First Game/Assets/Scripts/Assignments/Variables/Variables.cs
+++ First Game/Assets/Scripts/Assignments/Variables/Variables.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Variables : MonoBehaviour
 {
+    public float amount;
+    public float money;
+    public float cost;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cookies(amount,money,cost);     
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    void Cookies(float amount, float money, float cost)
+
+    {
+        float total;
+
+       total= money - cost * amount;
+
+        Debug.Log(total);
     }
 }
