4d6099a9753e9736301fccba4fea78e7a4407b29
Stefani <stefani.olson@gmail.com>
2020-02-24 14:49:38 -0700

    created new unity project

---
+++ Start Fresh/Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    public GameObject player;
+    public Transform target;
+    public int damage;
+    public float moveSpeed;
+    
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target);
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+    }
+}
---
+++ Start Fresh/Assets/Scripts/Health_Pickup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health_Pickup : Pickup
+{
+    public int healthAmt = 100;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Player is health is " + healthAmt);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("You gained  " + healthAmt+ " health!");
+        Destroy(gameObject);
+    }
+
+
+
+}
